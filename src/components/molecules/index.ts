fd0bf30ea8324c41ab5848f3438c78fa56aab631
Lam Nguyen <languyen@axon.com>
2025-11-02 22:32:22 +0700

    bump version

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,6 +1,5 @@
 export * from './Banner';
 export * from './Card';
-export * from './CookieBanner';
 export * from './horizontalTileCard';
 export * from './Modal';
-export * from './SignUpBanner';
+export * from './CategoryCard';
